64fba9813c10554cfd42e98febde001fc8d8bd36
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-11 12:09:29 +0200

    Update invite.js

--- src/interactions/Command/invite.js
+++ src/interactions/Command/invite.js
@@ -30,7 +30,7 @@ module.exports = {
 
         client.embed({
             title: `📨・Invite`,
-            desc: `Make your server even better with Bot!`,
+            desc: `❌ Sorry This Bot is Private And Made Specifically For The Families But You Can Invite Faky's Muisc`,
             image: "https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg",
             url: client.config.discord.botInvite,
             components: [row],
