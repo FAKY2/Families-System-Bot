68939b0c09a880c0483ddf760c04248728e4a94e
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-11 03:21:53 +0200

    Update invite.js

--- src/interactions/Command/invite.js
+++ src/interactions/Command/invite.js
@@ -15,12 +15,28 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         await interaction.deferReply({ fetchReply: true });
+        let row = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setLabel("Invite")
+                    .setURL(client.config.discord.botInvite)
+                    .setStyle(Discord.ButtonStyle.Link),
+
+                new Discord.ButtonBuilder()
+                    .setLabel("Support server")
+                    .setURL(client.config.discord.serverInvite)
+                    .setStyle(Discord.ButtonStyle.Link),
+            );
 
         client.embed({
             title: `📨・Invite`,
-            desc: `❌ Sorry, This bot Is Privet And Designed Specifically For The Families.`,
+            desc: `Make your server even better with Bot!`,
             image: "https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg",
+            url: client.config.discord.botInvite,
+            components: [row],
             type: 'editreply'
         }, interaction)
     },
 };
+
+ 
