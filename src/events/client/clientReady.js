4cca28c67ce3d62d650edfd4a4dee935962379fd
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-14 20:57:47 +0200

    Update clientReady.js

--- src/events/client/clientReady.js
+++ src/events/client/clientReady.js
@@ -40,7 +40,7 @@ module.exports = async (client) => {
                         `Families`,
                         `👀`,
                         `Families `,
-                        `eid mubarak 🌙`,
+                        `Families`,
                         `👀`
                     ];
                 }
