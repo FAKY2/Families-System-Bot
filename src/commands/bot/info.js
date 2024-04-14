cf26d0fcde5c9a530d225de9cdaa3bb0df4b1fab
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-04-14 20:56:48 +0200

    Update info.js

--- src/commands/bot/info.js
+++ src/commands/bot/info.js
@@ -131,7 +131,7 @@ module.exports = async (client, interaction, args) => {
                 },
                 {
                     name: "🔗┆Links",
-                    value: `Add me: [[HERE]](${client.config.discord.botInvite}) \nSupport server: [[HERE]](${client.config.discord.serverInvite})`,
+                    value: `FAKY's Music: [[INVITE]](${client.config.discord.botInvite}) \nSupport server: [[HERE]](${client.config.discord.serverInvite})`,
                     inline: false,
                 }],
                 type: 'editreply'
