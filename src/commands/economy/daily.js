d0ba8e5ec337b713d00c5eafd9c201f269e2b012
FAKY2 <161999344+FAKY2@users.noreply.github.com>
2024-05-29 13:42:04 +0300

    Update daily.js

--- src/commands/economy/daily.js
+++ src/commands/economy/daily.js
@@ -1,50 +1,46 @@
 const Discord = require('discord.js');
-
 const Schema = require("../../database/models/economy");
 const Schema2 = require("../../database/models/economyTimeout");
 
 module.exports = async (client, interaction, args) => {
-  let user = interaction.user;
-  let timeout = 86400000;
-  let amount = 200;
+    let user = interaction.user;
+    let timeout = 86400000; // 24 hours in milliseconds
 
-  Schema2.findOne({ Guild: interaction.guild.id, User: user.id }, async (err, dataTime) => {
-    if (dataTime && dataTime.Daily !== null && timeout - (Date.now() - dataTime.Daily) > 0) {
-      let time = (dataTime.Daily / 1000 + timeout / 1000).toFixed(0);
-      return client.errWait({
-        time: time,
-        type: 'editreply'
-      }, interaction);
-    }
-    else {
+    // Generate a random amount between 2000 and 4000
+    let amount = Math.floor(Math.random() * (4000 - 2000 + 1)) + 2000;
 
-      client.succNormal({
-        text: `You've collected your daily reward!`,
-        fields: [
-          {
-            name: `${client.emotes.economy.coins}┆Amount`,
-            value: `$${amount}`,
-            inline: true
-          }
-        ],
-        type: 'editreply'
-      }, interaction);
+    Schema2.findOne({ Guild: interaction.guild.id, User: user.id }, async (err, dataTime) => {
+        if (dataTime && dataTime.Daily !== null && timeout - (Date.now() - dataTime.Daily) > 0) {
+            let time = (dataTime.Daily / 1000 + timeout / 1000).toFixed(0);
+            return client.errWait({
+                time: time,
+                type: 'editreply'
+            }, interaction);
+        } else {
+            client.succNormal({
+                text: `You've collected your daily reward!`,
+                fields: [
+                    {
+                        name: `${client.emotes.economy.coins}┆Amount`,
+                        value: `$${amount}`,
+                        inline: true
+                    }
+                ],
+                type: 'editreply'
+            }, interaction);
 
-      if (dataTime) {
-        dataTime.Daily = Date.now();
-        dataTime.save();
-      }
-      else {
-        new Schema2({
-          Guild: interaction.guild.id,
-          User: user.id,
-          Daily: Date.now()
-        }).save();
-      }
+            if (dataTime) {
+                dataTime.Daily = Date.now();
+                dataTime.save();
+            } else {
+                new Schema2({
+                    Guild: interaction.guild.id,
+                    User: user.id,
+                    Daily: Date.now()
+                }).save();
+            }
 
-      client.addMoney(interaction, user, amount);
-    }
-  })
+            client.addMoney(interaction, user, amount);
+        }
+    })
 }
-
- 
